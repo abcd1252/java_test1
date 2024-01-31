6dccb7679ebb248ada253eba831bf59b96a1ff19
abcd1252 <153148788+abcd1252@users.noreply.github.com>
2024-01-31 13:09:11 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+    // 주석
+    /*
+    여러 주석
+     */
+
+    /*
+    * command - space : 자동완성 기능
+    * command - d : 복사
+     */
+
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다");
+
+    }
+}
